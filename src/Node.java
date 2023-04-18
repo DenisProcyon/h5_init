844c5965f7ade5b404fbb006b6572524df27091e
DenisProcyon <denis.shadrin@procyon.ee>
2023-04-18 18:22:54 +0300

    update

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class Node {
@@ -8,22 +7,51 @@ public class Node {
    private Node nextSibling;
 
    Node (String n, Node d, Node r) {
-      // TODO!!! Your constructor here
+      name = n;
+      firstChild = d;
+      nextSibling = r;
    }
-   
+
    public static Node parsePostfix (String s) {
-      return null;  // TODO!!! return the root
+      Stack<Node> stack = new Stack<Node>();
+      for (int i = s.length() - 1; i >= 0; i--) { //starting from end
+         char c = s.charAt(i);
+         if (c == ')') {
+            stack.push(null); // null represents that we are done dealing with siblings of current node
+         } else if (c == ',') {
+            continue;
+         } else { // if its anything except "," and ")" it means there is node
+            Node child = stack.pop();
+            Node sibling = stack.pop();
+            // we take two last elements of stack
+            if (sibling != null) { // in this case we are dealing with root because it has no siblings
+               sibling.nextSibling = child;
+            }
+            stack.push(new Node(String.valueOf(sibling.name), child, sibling.nextSibling));
+         }
+      }
+      return stack.pop(); //last element is obviously root
    }
 
    public String leftParentheticRepresentation() {
-      return ""; // TODO!!! return the string without spaces
+      StringBuilder sb = new StringBuilder();
+      sb.append(name);
+      if (firstChild != null) {
+         sb.append("(");
+         sb.append(firstChild.leftParentheticRepresentation());
+         sb.append(")");
+      }
+      if (nextSibling != null) {
+         sb.append(nextSibling.leftParentheticRepresentation());
+      }
+      return sb.toString();
    }
 
    public static void main (String[] param) {
-      String s = "(B1,C)A";
-      Node t = Node.parsePostfix (s);
-      String v = t.leftParentheticRepresentation();
-      System.out.println (s + " ==> " + v); // (B1,C)A ==> A(B1,C)
+      // after I tried it for 2 hours, saving siblings and children in
+      // variables, I asked chatGPT how to deal with it
+      // he told me that stack is a possible solution
+
+      // code itself is done by me though
    }
 }
-
